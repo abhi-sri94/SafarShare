09ce0763f46956ee3d69d3ab5ad374c67d3e80b1
Abhishek <user@users-MacBook-Pro.local>
2026-03-23 00:56:24 +0530

    Allow local dev origins (5500-5502) for Socket.io CORS

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -17,6 +17,12 @@ const initSocket = (server) => {
     const defaults = [
       'http://localhost:3000',
       'http://127.0.0.1:3000',
+      'http://localhost:5500',
+      'http://127.0.0.1:5500',
+      'http://localhost:5501',
+      'http://127.0.0.1:5501',
+      'http://localhost:5502',
+      'http://127.0.0.1:5502',
       'https://safarshare-web.vercel.app',
       'https://app.safarshare.in',
     ];
